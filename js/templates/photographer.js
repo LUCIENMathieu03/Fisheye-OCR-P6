8ddeb7271f59dcd47afcb5bb923e18a3ce0a534d
Lucien Mathieu <lucien.mathieu972@gmail.com>
2023-07-09 00:45:59 +0200

    feat - integrating the home page & adding all the img

--- js/templates/photographer.js
+++ js/templates/photographer.js
@@ -1,16 +1,23 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
+    const location = `${city}, ${country}`;
+    const cost = `${price}/jour`
+    const photographerDescription  = `<p class='firstLine'>${location}</p> <p>${tagline}</p> <p class="price">${cost}</p>`
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
+        const description = document.createElement('div');
+        description.innerHTML = photographerDescription;
+        description.classList.add("description")
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(description)
         return (article);
     }
     return { name, picture, getUserCardDOM }
